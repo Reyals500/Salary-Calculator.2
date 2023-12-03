efe0eb2195f68f71ae4f778cb9f27cd648c12448
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2023-12-03 12:03:11 -0600

    Currently trying to solve the problem of calculating the monthly salary

--- salary.js
+++ salary.js
@@ -1,9 +1,9 @@
-console.log('Make sure this');
+console.log('Make sure this works');
 
 function workSubmit(event){
     event.preventDefault();
     let tableBody = document.getElementById('tableBody');
-    
+
     let firstName = document.getElementById('firstName').value;
     let lastName = document.getElementById('lastName').value;
     let idNumber = document.getElementById('idNumber').value;
@@ -15,7 +15,6 @@ function workSubmit(event){
     console.log("Expecting custom id #:",idNumber);
     console.log("Expecting custom job title:",jobTitle);
     console.log("Expecting custom salary:",annualSalary);
-    
     tableBody.innerHTML += //coming up with Uncaught TypeError: Cannot read properties of undefined (reading 'innerHTML')
     `
     <tr>
@@ -24,12 +23,34 @@ function workSubmit(event){
         <td>${idNumber}</td>
         <td>${jobTitle}</td>
         <td>${annualSalary}</td>
+        <td><button onclick="deleteEmployee(event)">Delete</button></td>
     </tr>
     `
-
+// todo this element is meant to accept the inputs that I put in
     document.getElementById('firstName') = ''
     document.getElementById('lastName') = ''
     document.getElementById('idNumber') = ''
     document.getElementById('jobTitle') = ''
     document.getElementById('annualSalary') = ''
+
+    //attempting to get the monthly count in
+    let monthlySalary = document.getElementById('total-salary')
+    monthlySalary.innerHTML += 
+    
+}
+function deleteEmployee(event) {
+    event.preventDefault();
+    return event.target.parentElement.parentElement.remove();
+  }
+//   let monthlySalary = 0
+// function calculateSalary(event){
+//     console.log(monthlySalary += document.getElementById('annualSalary').value / 12)
+//     console.log(Math.trunc(monthlySalary));
+//     return event.target
+// }
+
+function alertSalary() {
+    if (monthlySalary > 20000) {
+        console.log('ALERT WE ARE OVER BUDGET')
+    }
 }
